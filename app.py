ca3652e49225088a9ec05a20872969bc70062f26
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2025-12-10 19:04:25 +0100

    ...

--- app.py
+++ app.py
@@ -152,9 +152,9 @@ def load_rag_pipeline(model_id):
                     llm = HuggingFaceEndpoint(
                         repo_id=model_id,
                         huggingfacehub_api_token=hf_token,
-                        task="conversational",
+                        task="text-generation",
                         temperature=0.5,
-                        max_new_tokens=1024,
+                        max_new_tokens=512,
                         top_p=0.9
                     )
                     
